1bef4212da8938f7862d3ffd422df6b2be4b7abb
AYANE KIKUCHI <a-kikuchi@unext.jp>
2020-04-24 21:54:10 +0900

    solve exercises of chap.13 in jissen

---
+++ java/sukkiri-wakaru-java/src/com/example/jissen/practice/chap13/Bank.java
@@ -0,0 +1,20 @@
+package com.example.jissen.practice.chap13;
+
+public class Bank {
+    /** 名前 */
+    String name;
+    /** 住所 */
+    String address;
+
+    /**
+     * 口座を作成する
+     * @param owner 口座名義人
+     * @param initialBalance 初期残高
+     */
+    public void addAccount(String owner, int initialBalance) {
+    }
+
+    public static void main(String args[]) {
+        System.out.println("試験用のメインメソッドです");
+    }
+}
